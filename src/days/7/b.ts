16c1fa5043ec6e6e4ba6092a3bd59a1be43b4571
Matt Tingen <matt.tingen@gmail.com>
2021-12-15 00:30:55 -0500

    add reference for rounding oddity
    I wasn't sure why `Math.round` didn't work. This paper explains it by deriving the non-integer solution. It follows that the integer solution must lie with the range [mean-0.5,mean+0.5], but there's no guarantee that rounding will provide the correct value.

--- src/days/7/b.ts
+++ src/days/7/b.ts
@@ -6,6 +6,7 @@ const cost = (distance: number) => (distance * (distance + 1)) / 2;
 
 export const b = (positions: number[]) => {
   const mean = arithmaticMean(positions);
+  // See https://old.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/
   const xCandidates = uniq([Math.floor(mean), Math.ceil(mean)]);
 
   return Math.min(
