1e604110622a8efc9adc6c21a278afe020419815
Matt Tingen <matt.tingen@gmail.com>
2022-12-03 14:26:35 -0500

    Add 2022 snapshots

---
+++ src/2022/challenges.test.ts
@@ -0,0 +1,7 @@
+import { createChallengeMacro } from '~/createChallengeMacro';
+
+const testChallenge = createChallengeMacro(2022);
+
+run(testChallenge, 1, 'a');
+run(testChallenge, 1, 'b');
+run(testChallenge, 2, 'a');
