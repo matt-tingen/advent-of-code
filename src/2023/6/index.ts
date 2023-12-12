c2529d67308f88a342fda5f05987690e11f1226e
Matt Tingen <matt.tingen@gmail.com>
2023-12-11 23:31:07 -0500

    6b

--- src/2023/6/index.ts
+++ src/2023/6/index.ts
@@ -1,8 +1,13 @@
-import { flow, range, unzip } from 'lodash';
+import { flow, unzip } from 'lodash';
 import { product } from '~/util/product';
 import { toInt } from '~/util/toInt';
 
-const parse = (input: string) => {
+interface Race {
+  duration: number;
+  recordDistance: number;
+}
+
+const parse = (input: string): Race[] => {
   const lines = input
     .split('\n')
     .map((line) => line.split(':')[1].trim().split(/\s+/).map(toInt));
@@ -15,12 +20,44 @@ const parse = (input: string) => {
   return races;
 };
 
-export const a = flow(parse, (races) => {
-  const winningStrategies = races.map((race) =>
-    range(race.duration)
-      .map((speed) => speed * (race.duration - speed))
-      .filter((distance) => distance > race.recordDistance),
-  );
+const isWinningStrategy = (race: Race, windTime: number) =>
+  windTime * (race.duration - windTime) > race.recordDistance;
 
-  return product(winningStrategies.map((strats) => strats.length));
-});
+const countWinningStrategies = (race: Race) => {
+  const { duration, recordDistance } = race;
+  // Via quadratic equation `distance = speed * (duration - windTime)`
+  // where `speed = windTime`. The negative solution is discarded.
+  const windTimeToMatchRecord =
+    0.5 * (duration + Math.sqrt(duration ** 2 - 4 * recordDistance));
+
+  let numberOfWinningStrategies = 0;
+  // Largest int strictly less than `windTimeToMatchRecord`
+  let windTime = Math.ceil(windTimeToMatchRecord - 1);
+
+  while (isWinningStrategy(race, windTime)) {
+    numberOfWinningStrategies++;
+    windTime--;
+  }
+
+  // Smallest int strictly greater than `windTimeToMatchRecord`
+  windTime = Math.floor(windTimeToMatchRecord + 1);
+
+  while (isWinningStrategy(race, windTime)) {
+    numberOfWinningStrategies++;
+    windTime++;
+  }
+
+  return numberOfWinningStrategies;
+};
+
+export const a = flow(parse, (races) =>
+  product(races.map(countWinningStrategies)),
+);
+
+export const b = flow((input: string): Race => {
+  const [duration, recordDistance] = input
+    .split('\n')
+    .map((line) => toInt(line.split(':')[1].replaceAll(' ', '')));
+
+  return { duration, recordDistance };
+}, countWinningStrategies);
