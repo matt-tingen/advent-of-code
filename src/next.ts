24c3789bf01d8d8bb3610d826cc3738b65df66bf
Matt Tingen <matt.tingen@gmail.com>
2023-12-13 21:36:14 -0500

    Add template to next command

--- src/next.ts
+++ src/next.ts
@@ -1,13 +1,25 @@
 import fs from 'fs/promises';
 import path from 'path';
+import dedent from 'dedent';
 import { getNextSolutionsFilePath } from './run';
+
+const solutionTemplate = dedent`
+  import { flow } from 'lodash';
+
+  const parse = (input: string) => {
+    //
+  };
+
+  export const a = flow(parse, (value) => {
+    //
+  });`;
 
 const main = async () => {
   const solutionsPath = await getNextSolutionsFilePath();
   const dayPath = path.dirname(solutionsPath);
 
   await fs.mkdir(dayPath, { recursive: true });
-  await fs.writeFile(solutionsPath, '');
+  await fs.writeFile(solutionsPath, `${solutionTemplate}\n`);
   await fs.writeFile(path.join(dayPath, 'input.txt'), '');
 };
 
