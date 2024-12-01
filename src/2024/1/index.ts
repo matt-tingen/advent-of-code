5b3196a85ba48f01b9fef97a1f66c5aefec74311
Matt Tingen <matt.tingen@gmail.com>
2024-12-01 13:42:04 -0500

    1b

--- src/2024/1/index.ts
+++ src/2024/1/index.ts
@@ -1,13 +1,21 @@
-import { flow, sortBy, sumBy, unzip, zip } from 'lodash';
+import { countBy, flow, sortBy, sumBy, unzip, zip } from 'lodash';
 import { parseDecimalInt, parseStringLines } from '~/util/parsers';
 
-const parse = flow(parseStringLines, (lines) =>
-  lines.map((line) => line.split('   ').map(parseDecimalInt)),
+const parse = flow(
+  parseStringLines,
+  (lines) => lines.map((line) => line.split('   ').map(parseDecimalInt)),
+  unzip,
 );
 
-export const a = flow(parse, (ids: [number, number][]) => {
-  const sorted = unzip(ids).map((list) => sortBy(list)) as [number[], number[]];
+export const a = flow(parse, (ids) => {
+  const sorted = ids.map((list) => sortBy(list)) as [number[], number[]];
   const zipped = zip(...sorted) as [number, number][];
 
   return sumBy(zipped, ([l, r]) => Math.abs(l - r));
+});
+
+export const b = flow(parse, ([left, right]) => {
+  const counts = countBy(right);
+
+  return sumBy(left, (id) => id * (counts[id] ?? 0));
 });
--- src/2024/challenges.test.ts
+++ src/2024/challenges.test.ts
@@ -3,3 +3,4 @@ import { createChallengeMacro } from '~/createChallengeMacro';
 const testChallenge = createChallengeMacro(2024);
 
 run(testChallenge, 1, 'a');
+run(testChallenge, 1, 'b');
